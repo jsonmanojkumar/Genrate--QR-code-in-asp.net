63c9c20199916e5ba64125b55426244fdc1639a6
jsonmanojkumar <jsonmanojkumar@gmail.com>
2020-03-31 00:46:10 -0700

    first

---
+++ Default.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <table class="auto-style1">
+            <tr>
+                <td>
+                    <asp:Label ID="Label1" runat="server" Text="enter your text"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td>
+                    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="genrate QR code" />
+                </td>
+            </tr>
+        </table>
+    <div>
+    
+        <%--<asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>--%>
+    
+    </div>
+        <asp:PlaceHolder ID="PlaceHolder1" runat="server"></asp:PlaceHolder>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using QRCoder;
+using System.IO;
+using System.Drawing;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string code = TextBox1.Text;
+        QRCodeGenerator qrGenerator = new QRCodeGenerator();
+        QRCodeGenerator.QRCode qrCode = qrGenerator.CreateQrCode(code, QRCodeGenerator.ECCLevel.Q);
+        System.Web.UI.WebControls.Image imgBarCode = new System.Web.UI.WebControls.Image();
+        imgBarCode.Height = 150;
+        imgBarCode.Width = 150;
+        using (Bitmap bitMap = qrCode.GetGraphic(20))
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bitMap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                byte[] byteImage = ms.ToArray();
+                imgBarCode.ImageUrl = "data:image/png;base64," + Convert.ToBase64String(byteImage);
+            }
+            PlaceHolder1.Controls.Add(imgBarCode);
+        }
+        //Label3.Visible = true;
+    }
+}
